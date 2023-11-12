8782d36ada659a14d449fc64a7f152c9ba4e885c
kaykRodr1gu3s <110197812+kaykRodr1gu3s@users.noreply.github.com>
2023-11-12 20:33:10 -0300

    .

--- Linux/linux_enterprise.py
+++ Linux/linux_enterprise.py
@@ -1,27 +1,40 @@
 import requests 
 import bs4
 import os 
+import re
 
 
 
-requisicao = requests.get('https://attack.mitre.org/matrices/enterprise/linux/')
+req = requests.get('https://attack.mitre.org/matrices/enterprise/linux/')
 
-bs = bs4.BeautifulSoup(requisicao.content, 'html.parser')
+bs = bs4.BeautifulSoup(req.content, 'html.parser')
 
 tatic_name = bs.find_all('td', {'class': 'tactic name'})
 href_tatic = []
 
-print(tatic_name)
 
-
-# for c in tatic_name.href:
-#     print(c)
+for c in tatic_name:
+    href_tatic.append(c.a['href'])
 
 
 number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-tatic_names = ['Initial Access', 'Execution', 'Persistence', 'Privilege Escalation', 'Defense Evasion', 'Credential Access','Discovery', 'Lateral Movement', 'Collection', 'Command and Control', 'Exfiltration', 'Impact']
+tatic_names = ['Initial Access', 'Execution', 'Persistence', 
+               'Privilege Escalation', 'Defense Evasion', 'Credential Access',
+               'Discovery', 'Lateral Movement', 'Collection',
+                 'Command and Control', 'Exfiltration', 'Impact']
 
 
+zipped = zip(number, tatic_names)
+
+for zip in zipped:
+    print(zip)
+
+print('-=' * 20)
+choice = int(input('qual das opçoes vc quer ?'))
+print('-=' * 20)
 
 
+count_techniques = bs.find('div', {'class': 'matrix-container p-3'})
+count_techniques = count_techniques.find_all('td', {'class' : 'tactic count'})
 
+print(count_techniques)
