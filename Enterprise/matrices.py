9f33fbb4f2a3476d8ac65e5c9fb40e3473ab1f33
kaykRodr1gu3s <110197812+kaykRodr1gu3s@users.noreply.github.com>
2023-11-13 23:27:14 -0300

    .

--- Enterprise/matrices.py
+++ Enterprise/matrices.py
@@ -3,49 +3,104 @@ import bs4
 import os
 import csv
 
-
-def Linux():
-    req = requests.get('https://attack.mitre.org/matrices/enterprise/linux/')
-
-
-
-    bs = bs4.BeautifulSoup(req.content, 'html.parser')
-
-    table = bs.find('table', {'class': 'matrix flat'})
-
-    tactics_name = table.find_all('td', {'class': 'tactic name'})
-
-    names = []
-    count = []
-
-    for c in tactics_name:
-        names.append(c.text)
-
-
-    tech_count = table.find_all('td', {'class': 'tactic count'})
-
-    for c in tech_count:
-        count.append(c.text.replace(u'\xa0', u' ').replace('\n', '').strip())
-
-    dict_names = {}
-
-    for k, v in enumerate(names):
-        dict_names[names[k]] = count[k]
-
+def writer(option):
 
 
     os.chdir(f'{os.getcwd()}\\Enterprise')
+    op = option
+    
+    
+    if op in 'cloud':
+        current = os.getcwd() + '\\Matrices\\cloud'
+   
+        cloud_plataform = ['office365', 'azuread', 'googleworkspace', 'saas', 'iaas']
+        for plataform in cloud_plataform:
+            req = requests.get(f'https://attack.mitre.org/matrices/enterprise/cloud/{plataform}/')
 
-    current = os.getcwd() + '\\Matrices'
+            print(req)
+
+
+            bs = bs4.BeautifulSoup(req.content, 'html.parser')
+
+            table = bs.find('table', {'class': 'matrix flat'})
+
+            tactics_name = table.find_all('td', {'class': 'tactic name'})
+
+            names = []
+            count = []
+
+            for c in tactics_name:
+                names.append(c.text)
+
+
+            tech_count = table.find_all('td', {'class': 'tactic count'})
+
+            for c in tech_count:
+                count.append(c.text.replace(u'\xa0', u' ').replace('\n', '').strip())
+
+            dict_names = {} 
+
+            for k, v in enumerate(names):
+                dict_names[names[k]] = count[k]
 
 
 
-    with open(f'{current}\\Linux.csv', 'w', newline='') as f:
-        csv_writer = csv.writer(f)
-        csv_writer.writerow(dict_names.keys())
-        csv_writer.writerow(dict_names.values())
+            with open(f'{current}\\{plataform}.csv', 'w', newline='') as f:
+                csv_writer = csv.writer(f)
+                csv_writer.writerow(dict_names.keys())
+                csv_writer.writerow(dict_names.values())
 
 
-Linux()
+
+    else:
+        current = os.getcwd() + '\\Matrices'
+
+        req = requests.get(f'https://attack.mitre.org/matrices/enterprise/{option}/')
+
+        print(req)
 
 
+        bs = bs4.BeautifulSoup(req.content, 'html.parser')
+
+        table = bs.find('table', {'class': 'matrix flat'})
+
+        tactics_name = table.find_all('td', {'class': 'tactic name'})
+
+        names = []
+        count = []
+
+        for c in tactics_name:
+            names.append(c.text)
+
+
+        tech_count = table.find_all('td', {'class': 'tactic count'})
+
+        for c in tech_count:
+            count.append(c.text.replace(u'\xa0', u' ').replace('\n', '').strip())
+
+        dict_names = {} 
+
+        for k, v in enumerate(names):
+            dict_names[names[k]] = count[k]
+
+
+        with open(f'{current}\\{option}.csv', 'w', newline='') as f:
+            csv_writer = csv.writer(f)
+            csv_writer.writerow(dict_names.keys())
+            csv_writer.writerow(dict_names.values())
+
+
+name = ['pre', 'windows', 'macos', 'cloud', 'network', 'containers']
+options = [0, 1, 2, 3, 4, 5]
+
+zipped = zip(options, name)
+
+
+for zipp in zipped:
+    print(zipp)
+
+print('-=' * 20)
+option = int(input('Which do you wanna see ? '))
+print('-=' * 20)
+
+writer(name[option])
